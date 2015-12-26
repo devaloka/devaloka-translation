ed77db04122185a6bf3f3a80b3a422b5ed50b75e
Whizark <devaloka@whizark.com>
2015-12-27 00:07:55 +0900

    fix: incorrect version number in plugin header comment

--- loader/10-devaloka-translation-loader.php
+++ loader/10-devaloka-translation-loader.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Devaloka Translation
 Description: Provides an implementation of Symfony TranslationInterface for WordPress
-Version: 0.2.0
+Version: 0.2.1
 Author: Whizark
 Author URI: http://whizark.com
 License: GPL-2.0+
