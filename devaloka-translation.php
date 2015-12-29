2b2a2f20a8e04cfc8a2973232386138e9ae8651d
Whizark <devaloka@whizark.com>
2015-12-29 21:04:21 +0900

    chore: bump version to 0.2.2

--- devaloka-translation.php
+++ devaloka-translation.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Devaloka Translation
 Description: Provides an implementation of Symfony TranslationInterface for WordPress
-Version: 0.2.1
+Version: 0.2.2
 Author: Whizark
 Author URI: http://whizark.com
 License: GPL-2.0+
